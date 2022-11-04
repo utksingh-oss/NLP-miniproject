08c0e1f01e974d301c934c8f9a1bd409615ba9c5
utkarsh singh <utk608@gmail.com>
2022-11-04 11:55:09 +0530

    made changes to get_analysis and main file

--- get_analysis.py
+++ get_analysis.py
@@ -1,6 +1,7 @@
 import spacy
 from spacy.matcher import Matcher
-from vaderSentiment.vaderSentiment import SentimentIntesityAnalyzer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
 
 
 # for obtaining data from DB
@@ -10,7 +11,7 @@ diary_entry_helper = DiaryEntryHelper()
 
 nlp = spacy.load("en_core_web_sm")
 
-expenditur_terms = ['buy','transfer','spend','expense','invest','loan','loss','charge','debit','decrement','use','give','lend']
+expenditure_terms = ['buy','transfer','spend','expense','invest','loan','loss','charge','debit','decrement','use','give','lend']
 savings_terms = ['receive','gain','acquire','credit','borrowed']
 
 # MATCHER FUNCTIONS ============================================================================================================
@@ -39,178 +40,273 @@ money_pattern.append([{"POS" : "NUM"},{"DEP":"nmod"}])
 
 matcher.add("money", patterns = money_pattern)
 
+
+#UTIL FUNCTIONS ===============================================================================================================
+			
+def get_children_text(txt):
+  lst = {}
+  
+  lst[txt.text] = txt.idx
+  
+  for child in txt.children:
+    rec_lst = get_children_text(child)
+    lst.update(rec_lst)
+  return lst
+
+def sort_dict(mydict):
+  marklist=list(mydict.items())
+  l=len(mydict)
+  for i in range(l-1):
+      for j in range(i+1,l):
+          if marklist[i][1]>marklist[j][1]:
+              t=marklist[i]
+              marklist[i]=marklist[j]
+              marklist[j]=t
+  sortdict=dict(marklist)
+  return sortdict
+
 # PART OF SPEECH TAGGING =======================================================================================================
 
 
 ## Utility Functions----------------------------
-def get_children_text(txt):
-	lst = {}
-	lst[txt.text] = txt.idx
-	for child in txt.children:
-		rec_lst = get_children_text(child)
-		lst.update(rec_lst)
-	return lst
 
-def sort_dict(myDict):
-	marklist = list(myDict.items())
-	l = len(myDict)
-	#performing bubble sort on list 
-	for i in range(l - 1):
-		for j in range(i + 1 , l):
-			if marklist[i][1] > marklist[i+1][1]:
-				t = marklist[i]
-				marklist[i] = marklist[j]
-				marklist[j] = t
-	sortdict = dict(marklist)
-	return sortdict
+#GET EXPENDITURE 
+def get_expenditure_table_1(text , date):
+  doc = nlp(text)
+  expenses = []
+  for sent in doc.sents:
+    # matcher_result = matcher(sent , as_spans = True)
+    for token in sent:
+      if token.lemma_ in expenditure_terms:
+        #check if their is negation in sentence if so break current loop
+        
+        for child in token.children:
+           curr_expense = {}
+           if (child.pos_ == "NOUN" or child.pos_ == "PROPN") and child.dep_ == "dobj":
 
+             curr_expense["spent on"] = child.text
+             curr_expense["date"] = date
+             list_of_string = get_children_text(token)
+             list_of_string = sort_dict(list_of_string)
+           
+             temp = []
+             for txt in list_of_string.keys():
+               temp.append(txt)
+             list_of_string = temp
+             list_of_string = ' '.join(list_of_string)
+             to_be_passed = nlp(list_of_string)
+             result = matcher(to_be_passed, as_spans = True)
+             if len(result) == 0:
+               curr_expense = None
+             else:
+               max_string = ""
+               for curr in result:
+                 if len(curr) > len(max_string):
+                   max_string = curr
+               curr_expense["spent"] = max_string.text
 
+             if(curr_expense == None): 
+               continue
+             else:
+               expenses.append(curr_expense)
+  print(expenses)
+  return expenses
 
-#Extracting Expenditure for Sentence type : 1
-#"I bought a house for seventy thousand dollars."
-def get_moneyflow_table_1(text , flow , list_of_terms):
-	doc = nlp(text)
-	if flow == 'outwards':
-		var_1 = "spent"
-		var_2 = "spent-on"
-	elif flow == 'inwards':
-		var_1 = "received"
-		var_2 = "received-from"
-	
-	total_flow = []
-	for sent in doc.sents:
-		for token in sent:
-			if token.lemma_ in list_of_terms:
-				curr_flow = {}
-				for child in token.children:
-					if child.pos_ == "NOUN" and child.dep_ =="dobj":
-						curr_flow[var_2] = child
-						list_of_string = get_children_text(child)
-						list_of_string = sort_dict(list_of_string)
+def get_expenditure_table_2(text , date):
+  doc = nlp(text)
+  expenses = []
+  for sent in doc.sents:
+    # matcher_result = matcher(sent, as_spans = True)
+    for token in sent:
+      if token.lemma_ in expenditure_terms:
+        for child in token.children:
+          for sub_child in child.children:
+            if (sub_child.pos_ == "NOUN" or sub_child.pos_=="PROPN") and sub_child.dep_ == "pobj":
+              
+              curr_expense = {}
+              curr_expense["spent on"] = sub_child.text
+              curr_expense["date"] = date
+              
+         
+              list_of_string = get_children_text(token)
+              
+              list_of_string = sort_dict(list_of_string)
+              
 
-						temp = []
-						for txt in list_of_string.keys():
-							temp.append(txt)
-						list_of_string = temp
-						list_of_string = ' '.join(list_of_string)
-						to_be_passed = nlp(list_of_string)
-						result = matcher(to_be_passed , as_spans = True)
-						if len(result) == 0:
-							curr_flow = None
-							continue
-						else:
-							max_string = ""
-							for curr in result:
-								if len(curr) > len(max_string):
-									max_string = curr
-							curr_flow[var_1] = max_string
+              temp = []
+              for txt in list_of_string.keys():
+                temp.append(txt)
+              list_of_string = temp
+              list_of_string = ' '.join(list_of_string)
+              to_be_passed = nlp(list_of_string)
+              result = matcher(to_be_passed , as_spans = True)
+              if len(result) == 0:
+                curr_expense = None
+              else:
+                max_string = ""
+                for curr in result:		
+                  if len(curr) > len(max_string):
+                    max_string = curr
+                curr_expense["spent"] = max_string.text
+                if(curr_expense == None):
+                  continue
+                else:
+                  expenses.append(curr_expense)
+  return expenses
 
-						curr_flow.append(curr_flow)
-	return total_flow
+def get_expenditure_table_3(text , date):
+  doc = nlp(text)
+  expenses = []
+  spending_term = False
+  for sent in doc.sents:
+    for token in sent:
+      if token.lemma_ in expenditure_terms:
+        spending_term = True
+    if not spending_term:
+      continue
+    curr_expense = {}
+    matcher_result = matcher(sent , as_spans = True)
+    max_string = ""
+    for res in matcher_result:
+      if len(res) > len(max_string):
+        max_string = res
+    curr_expense["spent"] = max_string.text
+    curr_expense["spent on"] = "NA"
+    curr_expense["date"] = date
+    expenses.append(curr_expense)
+  return expenses
+      
 
-#Extracting Expenditure for Sentence type : 2
-#"I spent fourty thousand dollars on a house."
-def get_moneyflow_table_2(text, flow, list_of_terms):
-	doc = nlp(text)
-	if flow == 'outwards':
-		var_1 = "spent"
-		var_2 = "spent-on"
-	elif flow == 'inwards':
-		var_1 = "received"
-		var_2 = "received-from"
-	total_flow = []
-	for sent in doc.sents:
-		for token in sent:
-			if token.lemma_ in list_of_terms:
-				curr_flow = {}
-				for child in token.children:
-					for sub_child in child.children:
-						if sub_child.pos_ == "NOUN" and sub_child == "pobj":
-							curr_flow[var_2] = sub_child
-							list_of_string = get_children_text(child)
-							list_of_string = sort_dict(list_of_string)
-
-							temp = []
-							for txt in list_of_string.keys():
-								temp.append(txt)
-							list_of_string = temp
-							list_of_string = ' '.join(list_of_string)
-							to_be_passed = nlp(list_of_string)
-							result = matcher(to_be_passed , as_spans = True)
-							if len(result) == 0:
-								curr_flow = None
-								continue
-							else:
-								max_string = ""
-								for curr in result:
-									if len(curr) > len(max_string):
-										max_string = curr
-								curr_flow[var_1] = max_string
-							total_flow.append(curr_flow)
-	return total_flow
-
-def get_moneyflow_table_3(text):
-	doc = nlp(text)
-	total_flow= []
-	flow_term_found = False
-	flow_term = ""
-	for sent in doc.sents:
-		for token in sent:
-			if token.lemma_ in expenditure_terms:
-				flow_term_found = True
-				flow_term = 'outwards'
-
-	for sent in doc.sents:
-		for token in sent:
-			if token.lemma_ in savings_terms:
-				flow_terms_found = False
-				flow_term = 'inwards'
-
-	if not flow_term_found:
-		return None
-
-	if flow_term == 'outwards':
-		var_1 = "spent"
-		var_2 = "spent-on"
-	elif flow_term == 'inwards':
-		var_1 = "received"
-		var_2 = "received-from"
+def get_expenditure(text  , date):
+  doc = nlp(text)
+  total_expenses = []
+  for sent in doc.sents:
+    
+    expenses = get_expenditure_table_1(sent.text , date)
+    if len(expenses) == 0:
+      expenses = get_expenditure_table_2(sent.text, date)
+    if len(expenses) == 0:
+      expenses = get_expenditure_table_3(sent.text, date)
+      print(expenses)
+    
+    total_expenses.append(expenses)
+  return total_expenses
 
 
-	for sent in doc.sents:	 
-		curr_flow = []
-		matcher_result = matcher(sent , as_spans = True)
-		max_string = ""
-		for res in matcher_result :
-			if len(res) > len(max_string):
-				max_string = res
-		curr_flow[var_1] = max_string
-		curr_flow[var_2] = None
-		total_flow.append(curr_flow)
-	return curr_flow
+# GET INCOME
+def get_income_table_1(text , date):
+  doc = nlp(text)
+  income = []
+  for sent in doc.sents:
+    matcher_result = matcher(sent , as_spans = True)
+    for token in sent:
+      if token.lemma_ in savings_terms:
+        #check if their is negation in sentence if so break current loop
+        curr_income = {}
+        for child in token.children:
+           if (child.pos_ == "NOUN" or child.pos_ == "PROPN") and child.dep_ == "dobj":
+             curr_income["received from"] = child.text
+             curr_income["date"] = date
+             list_of_string = get_children_text(token)
+             list_of_string = sort_dict(list_of_string)
+           
+             temp = []
+             for txt in list_of_string.keys():
+               temp.append(txt)
+             list_of_string = temp
+             list_of_string = ' '.join(list_of_string)
+             to_be_passed = nlp(list_of_string)
+             result = matcher(to_be_passed, as_spans = True)
+             if len(result) == 0:
+               curr_income = None
+             else:
+               max_string = ""
+               for curr in result:
+                 if len(curr) > len(max_string):
+                   max_string = curr
+               curr_income["received"] = max_string.text
 
-def get_moneyFlow(txt):
-	doc = nlp(txt)
-	lst = []
-	for sent in doc.sents:
-		curr_lst = get_moneyflow_table_1(sent, 'inwards', savings_terms)
-		if curr_lst == None:
-			curr_lst = get_moneyflow_table_1(sent , 'outwards' , expenditure_terms)
+             if(curr_income == None): 
+               continue
+             else:
+               income.append(curr_income)
+  return income
 
-		if curr_lst == None:
-			curr_lst = get_moneyflow_table_2(sent , 'inwards', savings_terms)
+def get_income_table_2(text , date):
+  doc = nlp(text)
+  income = []
+  for sent in doc.sents:
+    matcher_result = matcher(sent, as_spans = True)
+    for token in sent:
+      if token.lemma_ in savings_terms:
+        curr_income = {}
+        for child in token.children:
+          for sub_child in child.children:
+            if (sub_child.pos_ == "NOUN" or sub_child.pos_=="PROPN") and sub_child.dep_ == "pobj":
+              curr_income["received from"] = sub_child.text
+              curr_income["date"] = date
+              list_of_string = get_children_text(token)
+              list_of_string = sort_dict(list_of_string)
 
-		if curr_lst == None:
-			curr_lst = get_moneyflow_table_2(sent , 'outwards', expenditure_terms)
+              temp = []
+              for txt in list_of_string.keys():
+                temp.append(txt)
+              list_of_string = temp
+              list_of_string = ' '.join(list_of_string)
+              to_be_passed = nlp(list_of_string)
+              result = matcher(to_be_passed , as_spans = True)
+              if len(result) == 0:
+                curr_income = None
+              else:
+                max_string = ""
+                for curr in result:		
+                  if len(curr) > len(max_string):
+                    max_string = curr
+                curr_income["received"] = max_string.text
+                if(curr_income == None):
+                  continue
+                else:
+                  income.append(curr_income)
+  return income
 
-		if curr_lst == None:
-			curr_lst = get_moneyflow_table_3(sent)
+def get_income_table_3(text , date):
+  doc = nlp(text)
+  income = []
+  income_term = False
+  for sent in doc.sents:
+    for token in sent:
+      if token.lemma_ in savings_terms:
+        income_term = True
+    if not income_term:
+      continue
+    curr_income = {}
+    matcher_result = matcher(sent , as_spans = True)
+    max_string = ""
+    for res in matcher_result:
+      if len(res) > len(max_string):
+        max_string = res
+    curr_income["received"] = max_string.text
+    curr_income["date"] = date
+    curr_income["received from"] = "NA"
+    income.append(curr_income)
+  return income
+      
+		
 
-		if curr_lst == None:
-			continue
-		else:
-			lst.append(curr_lst)
-	return lst
+def get_income(text , date):
+  doc = nlp(text)
+  total_income = []
+  for sent in doc.sents:
+    print(sent)
+    income = get_income_table_1(sent.text , date)
+    if len(income) == 0 :
+      income = get_income_table_2(sent.text , date)
+      print("inside first if")
+    if len(income) == 0:
+      income = get_income_table_3(sent.text , date)
+      print("inside second if")
+    total_income.append(income)
+  return total_income
 
 
 #FINAL CLASS ============================================================================
@@ -221,11 +317,29 @@ class GetAnalysis:
 
 	def get_money_flow(self, month):
 		entries = diary_entry_helper.get_entry(month)
-		res = []
+		exp = []
+		print("The entries for the month are : ",entries)
 		for curr in entries:
-			temp = get_moneyFlow(curr)
-			res.append(temp)
+			print("this is to be passed : ", curr[3])
+			doc = nlp(curr[3])
+			for sent in doc.sents:
+				temp = get_expenditure(sent.text , curr[1])
+				if len(temp)!=0:
+					exp.append(temp)
+		
+		inc = []
+		for curr in entries:
+			print("this is to be passed : ", curr[3])
+			doc = nlp(curr[3])
+			for sent in doc.sents:
+				temp = get_income(sent.text , curr[1])
+				if len(temp)!= 0 :
+					inc.append(temp)
+		res = {}
+		res["expenses"] = exp
+		res["income"] = inc
+		print(res)
 		return res
 
-	def 
+
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from add_diary_entry import DiaryEntryHelper
 from get_analysis import GetAnalysis
+import pandas as pd
 
 selection = st.sidebar.radio("Navigation", ["Add Diary Entry","Get Analysis"])
 diary_entry_helper = DiaryEntryHelper()
@@ -26,8 +27,46 @@ elif selection == "Get Analysis":
 		# pass
 		get_analysis_object = GetAnalysis()
 		result_money = get_analysis_object.get_money_flow(month)
+		income = result_money["income"]
+		expense = result_money["expenses"]
+		st.write("Income")
+		inc_dates =[]
+		inc_amount=[]
+		inc_source=[]
+		for curr in income:
+			if curr and len(curr[0]) != 0:
+				inc_dates.append(curr[0][0]["date"])
+				inc_source.append(curr[0][0]["received from"])
+				inc_amount.append(curr[0][0]["received"])
+
+		inc_data = {
+			"dates": inc_dates,
+			"amount" : inc_amount,
+			"source" : inc_source
+		}
+		df1 = pd.DataFrame(inc_data)
+
+		st.write(df1)
+
+		st.write("Expenses")
+		exp_dates = []
+		exp_amount = []
+		exp_source = []
+		for curr in expense:
+			if curr and len(curr[0]) != 0 :
+				exp_dates.append(curr[0][0]["date"])
+				exp_amount.append(curr[0][0]["spent"])
+				exp_source.append(curr[0][0]["spent on"])
+
+		exp_data = {
+			"dates" : exp_dates,
+			"amount" : exp_amount,
+			"source" : exp_source
+		}
+		df2 = pd.DataFrame(exp_data)
+		st.write(df2)
+		# st.write(income)
+		# st.write(expense)
 		# result_sentiment = get_analysis_object.get_sentiment_analysis(month)
 		# st.write(result_money)
 		# st.write(result_sentiment)
-		result = diary_entry_helper.get_entry("November")
-		st.write(result_money)
